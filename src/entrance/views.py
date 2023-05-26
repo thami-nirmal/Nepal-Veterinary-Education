d7218902d8414d1d7430192018c23ee647cc6c38
nirmal <thaminirmal112@gmail.com>
2023-05-26 17:00:30 +0545

    data retrieve and to show them in template

--- src/entrance/views.py
+++ src/entrance/views.py
@@ -17,17 +17,28 @@ class GkView(View):
 
 class SyllabusInfoView(View):
     def get(self, request, *args, **kwargs):
-        template_name = 'entrance_prep_syllabus.html'
-        syllabus_info_object = SyllabusInfo.objects.all()
-        tu_syllabus_info_object = syllabus_info_object.filter(university_choices = 'TU').order_by('faculty_choices')
+        template_name                  = 'entrance_prep_syllabus.html'
+        syllabus_info_object           = SyllabusInfo.objects.filter(is_shown=True)
+        tu_syllabus_info_object        = syllabus_info_object.filter(university_choices = 'TU').order_by('faculty_choices')
         
-        print(syllabus_info_object)
-        syllabus_data = SyllabusInfo.objects.order_by('university_choices', ).values('university_choices', 'faculty_choices', 'subject', 'marks')
-        # print(syllabus_data.values())
         # print(syllabus_info_object)
+        # syllabus_data = SyllabusInfo.objects.order_by('university_choices', ).values('university_choices', 'faculty_choices', 'subject', 'marks')
+        # print(syllabus_data.values()
+        # print(syllabus_info_object)
+        group = {}
+        for data in syllabus_info_object:
+            if data.university_choices not in group:
+                group[data.university_choices] = {}
+            if data.faculty_choices not in group[data.university_choices]:
+                group[data.university_choices][data.faculty_choices] = []
+            syllabus_data = [data.subject, data.no_of_question, data.marks]
+            group[data.university_choices][data.faculty_choices].append(syllabus_data)
+
+        # print(group)
         context = {
             'data' : tu_syllabus_info_object,
             'syllabus_data' : syllabus_data,
+            'group': group,
         }
         return render(request, template_name, context)
     
